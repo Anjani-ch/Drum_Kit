ced52a4a900f46f1a75b48a9b99681630cbf44cd
Anjani <pranav.ch.12@outlook.com>
2022-01-22 19:57:18 +0100

    Initial Commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelector('#buttons');
+
+const playAudio = ref => {
+    const audio = new Audio(`../audio/${ref}.mp3`);
+    audio.play();
+}
+
+buttons.addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') playAudio(e.target.className);
+})
+
+window.addEventListener('keydown', e => Array.from(buttons.children).forEach(button => e.key == button.textContent ? playAudio(button.className) : ''))
